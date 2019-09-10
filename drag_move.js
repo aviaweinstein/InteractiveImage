71f338ff1654d1413f8290313a20daefa56f82dd
Avia Weinstein <aviaweinstein@gmail.com>
2019-09-09 21:52:47 -0700

    tweak/function_scope (#2)
    pulls function out to global scope

--- drag_move.js
+++ drag_move.js
@@ -1,37 +1,35 @@
-$(document).ready(function(){
-  function interactiveImage(pathToImage = 'assets/images/', fileName = 'image.jpg', totalFrames = 12, targetedImage = '#move') {
-    var frame = 0;
+function interactiveImage(pathToImage = 'assets/images/', fileName = 'image.jpg', totalFrames = 12, targetedImage = '#move') {
+  var frame = 0;
 
-    $(targetedImage).on('mousedown touchstart', function(e) {
-      e.preventDefault();
-      var newY = null;
-      var oldY = null;
+  $(targetedImage).on('mousedown touchstart', function(e) {
+    e.preventDefault();
+    var newY = null;
+    var oldY = null;
 
-      function changeFrame(event, frameSpeed) {
-        oldY = newY;
-        newY = event.pageY || event.originalEvent.touches[0].pageY;
-        if ((frame < totalFrames) & (oldY < newY) & (Math.ceil(newY)%frameSpeed == 0)) {
-          frame += 1;
-        } else if ((frame > 0) & (oldY > newY) & (Math.ceil(newY)%frameSpeed == 0)) {
-          frame -= 1;
-        }
-        $(targetedImage).first().attr('src', pathToImage + frame + '-' + fileName);
+    function changeFrame(event, frameSpeed) {
+      oldY = newY;
+      newY = event.pageY || event.originalEvent.touches[0].pageY;
+      if ((frame < totalFrames) & (oldY < newY) & (Math.ceil(newY)%frameSpeed == 0)) {
+        frame += 1;
+      } else if ((frame > 0) & (oldY > newY) & (Math.ceil(newY)%frameSpeed == 0)) {
+        frame -= 1;
       }
+      $(targetedImage).first().attr('src', pathToImage + frame + '-' + fileName);
+    }
 
-      $(targetedImage).on('mousemove', function(e) {
-        e.preventDefault();
-        changeFrame(e, 2);
-      });
-
-      $(targetedImage).on('touchmove',function(e) {
-        e.preventDefault();
-        changeFrame(e, 5);
-      });
-
-      $(targetedImage).on('mouseup touchend', function(e) {
-        e.preventDefault();
-        $(targetedImage).unbind('mousemove touchmove');
-      });
+    $(targetedImage).on('mousemove', function(e) {
+      e.preventDefault();
+      changeFrame(e, 2);
     });
-  }
-});
+
+    $(targetedImage).on('touchmove',function(e) {
+      e.preventDefault();
+      changeFrame(e, 5);
+    });
+
+    $(targetedImage).on('mouseup touchend', function(e) {
+      e.preventDefault();
+      $(targetedImage).unbind('mousemove touchmove');
+    });
+  });
+}
